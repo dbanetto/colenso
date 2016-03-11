18578819482f23f003d15629462b9670ac6933a3
David Barnett <david@barnett.net.nz>
2016-03-11 18:49:47 +1300

    refactor browse XQueries out of routes

--- db.js
+++ db.js
@@ -1,7 +1,57 @@
 var basex = require('basex');
+var cheerio = require('cheerio');
+var _ = require('underscore');
 
 var session = new basex.Session(process.env.BASEX_HOST || 'localhost',
                                 process.env.BASEX_PORT || 1984,
                                 process.env.BASEX_USER || 'admin',
                                 process.env.BASEX_PASS || 'admin');
+
+basex.Session.prototype.documentsInFolder = function(path, cb) {
+  this.execute("XQUERY <result> { for $c in collection('colenso" + path + "')\n return <li> { db:path($c) } </li> } </result> ",
+                function(err, data) {
+                  if (err) {
+                    cb(err);
+                    return;
+                  }
+                  var $ = cheerio.load(data.result);
+                  var list = [];
+                  $('li').each(function(i, elem) {
+                    list[i] = $(this).text();
+                  });
+                  cb(undefined, list);
+                });
+};
+
+basex.Session.prototype.foldersInPath = function(path, cb)  {
+  this.documentsInFolder(path, function(err, data) {
+    path = path.replace('/', '');
+    if (err) {
+      cb(err);
+      return;
+    }
+    var list = _.map(data, function(line) {
+      line = line.replace(path, '');
+      if (line.indexOf('/') > -1) {
+        line = line.replace(/^\//, '');
+        line = line.replace(/\/.*$/, '');
+      }
+      return line;
+    });
+
+    cb(undefined, _.uniq(list));
+  });
+};
+
+basex.Session.prototype.getDocument = function(path, cb) {
+  this.execute("XQUERY doc('colenso" + path + "')",
+                function(err, data) {
+                  if (err) {
+                    cb(err);
+                    return;
+                  }
+                  cb(undefined, data.result);
+                });
+};
+
 module.exports = session;
--- routes/browse.js
+++ routes/browse.js
@@ -1,39 +1,23 @@
 var express = require('express');
 var router = express.Router();
 var basex = require('../db');
-var cheerio = require('cheerio');
 
 /* GET search page. */
-router.get('/', function(req, res, next) {
-  basex.execute("list", function(err, data) {
-    console.log(err);
-    console.log(data);
-  });
-  res.render('browse', { title: 'Colenso' });
-});
 
 router.get('/*/view', function(req, res, next) {
   var url = req.url.replace(/\/view\/?/, '');
-  basex.execute("XQUERY doc('colenso" + url + ".xml')",
-                function(err, data) {
-                  if (err) console.log(err);
-                  console.log(data);
-                  res.render('view', { title: 'Colenso', doc: data.result  });
-                });
+  basex.getDocument(url + ".xml", function(err, doc) {
+    if (err) console.log(err);
+    res.render('view', { title: 'Colenso', doc: doc  });
+  });
 });
 
 // GET all files that are stored using this
 router.get('/*', function(req, res, next) {
-  basex.execute("XQUERY <result> { for $c in collection('colenso" + req.url + "')\n return <li> { db:path($c) } </li> } </result> ",
-                function(err, data) {
-                  if (err) console.log(err);
-                  var $ = cheerio.load(data.result);
-                  var list = [];
-                  $('li').each(function(i, elem) {
-                    list[i] = $(this).text();
-                  });
-                  res.render('browse', { title: 'Colenso', list: list });
-                });
+  basex.foldersInPath (req.url, function(err, data) {
+    if (err) console.log(err);
+    res.render('browse', { title: 'Colenso', list: data });
+  });
 });
 
 module.exports = router;
