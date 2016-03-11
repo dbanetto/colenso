6424866f8ecbc53df81d00e595cd91356500fb71
David Barnett <david@barnett.net.nz>
2016-03-11 15:44:57 +1300

    add viewing single document

--- routes/browse.js
+++ routes/browse.js
@@ -13,13 +13,18 @@ router.get('/', function(req, res, next) {
 });
 
 router.get('/*/view', function(req, res, next) {
-  console.log("View:" + req.params);
-  res.render('browse', { title: 'Colenso' });
+  var url = req.url.replace(/\/view\/?/, '');
+  basex.execute("XQUERY doc('colenso" + url + ".xml')",
+                function(err, data) {
+                  if (err) console.log(err);
+                  console.log(data);
+                  res.render('view', { title: 'Colenso', doc: data.result  });
+                });
 });
 
 // GET all files that are stored using this
 router.get('/*', function(req, res, next) {
-  basex.execute("XQUERY <result> { for $c in collection('colenso" + req.url + "')\n return <li> { document-uri($c) } </li> } </result> ",
+  basex.execute("XQUERY <result> { for $c in collection('colenso" + req.url + "')\n return <li> { db:path($c) } </li> } </result> ",
                 function(err, data) {
                   if (err) console.log(err);
                   var $ = cheerio.load(data.result);
